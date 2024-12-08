62593dd8fe02c17fb2b29e4e6aa5c97807bf177e
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2024-12-07 23:15:46 -0500

    Write a new contract using OpenZeppelin ERC721

---
+++ packages/hardhat/contracts/NFTCollection.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTCollection is ERC721, Ownable {
+    uint256 public nextTokenId;
+    string public baseTokenURI;
+
+    constructor(string memory _name, string memory _symbol, string memory _baseTokenURI) ERC721(_name, _symbol) {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function mint() public onlyOwner {
+        _safeMint(msg.sender, nextTokenId);
+        nextTokenId++;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseTokenURI;
+    }
+}
