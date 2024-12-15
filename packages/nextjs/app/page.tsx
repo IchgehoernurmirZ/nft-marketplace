c739a2597e8f428ab866e120b0c8d12eb41a891c
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2024-12-15 01:34:18 -0500

    add random nft feature

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -6,6 +6,7 @@ import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import UploadNFTForm from "~~/components/UploadNFTForm";
 import { Address } from "~~/components/scaffold-eth";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
